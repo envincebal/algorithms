2e97d255b012a32be190781f0fe1ef3f93bb1e69
envincebal <vincentyan1@gmail.com>
2018-12-14 20:48:44 -0800

    git commit -m "first commit"

---
+++ FreeCodeCamp/basic-algorithms/CelsToFahren.js
@@ -0,0 +1,11 @@
+/* The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32. */
+
+function convertToF(celsius) {
+  let fahrenheit = celsius * 9 / 5 + 32;
+
+  return fahrenheit;
+}
+
+const a = convertToF(30);
+
+console.log(a);

2c795f51d03693e4dd376c6ac1329df66708c333
envincebal <vincentyan1@gmail.com>
2019-01-31 18:09:39 -0800

    Added FCC Intermediate Algorithms folder and first solution

---
+++ FreeCodeCamp/intermediate-algorithms/SumNumbersInRange.js
@@ -0,0 +1,18 @@
+/* We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+
+The lowest number will not always come first. */
+
+function sumAll(arr) {
+  const minNum = Math.min(...arr);
+  const maxNum = Math.max(...arr);
+  let counter = 0;
+
+  for (let i = minNum; i <= maxNum; i++) {
+    counter += i;
+  }
+  return counter;
+}
+
+let a = sumAll([1, 4]);
+
+console.log(a);

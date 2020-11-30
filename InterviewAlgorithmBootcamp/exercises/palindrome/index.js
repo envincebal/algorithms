ab4ec143121ee5c3612ee003a0d4315831c5be87
envincebal <vincentyan1@gmail.com>
2020-11-30 01:11:05 -0800

    Added Palindrome solution.

--- InterviewAlgorithmBootcamp/exercises/palindrome/index.js
+++ InterviewAlgorithmBootcamp/exercises/palindrome/index.js
@@ -1,12 +1,29 @@
-// --- Directions
-// Given a string, return true if the string is a palindrome
-// or false if it is not.  Palindromes are strings that
-// form the same word if it is reversed. *Do* include spaces
-// and punctuation in determining if the string is a palindrome.
-// --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
+// --- Directions Given a string, return true if the string is a palindrome or
+// false if it is not.  Palindromes are strings that form the same word if it is
+// reversed. *Do* include spaces and punctuation in determining if the string is
+// a palindrome. --- Examples:   palindrome("abba") === true
+// palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let reversedString = [];
+  for (let index = str.length; index >= 0; index--) {
+    let splitString = str.split("");
+
+    reversedString.push(splitString[index]);
+  }
+  reversedString = reversedString.join("");
+
+  return str === reversedString ? true : false;
+
+}
+
+// Test cases
+let a = palindrome("abba");
+let b = palindrome("ffdffg");
+let c = palindrome("10101");
+
+console.log(a);
+console.log(b);
+console.log(c);
 
 module.exports = palindrome;
--- InterviewAlgorithmBootcamp/exercises/reversestring/index.js
+++ InterviewAlgorithmBootcamp/exercises/reversestring/index.js
@@ -10,10 +10,19 @@ function reverse(str) {
   const reversedString = [];
   for (let index = str.length; index >= 0; index--) {
     let splitString = str.split("");
-    
+
     reversedString.push(splitString[index]);
   }
   return reversedString.join("");
 }
+
+// Test cases
+let a =  reverse('apple');
+let b = reverse('hello');
+let c = reverse('Greetings!');
+
+console.log(a);
+console.log(b);
+console.log(c);
 
 module.exports = reverse;

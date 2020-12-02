583dc24c0d243498a70d565225e04c3efb9b183c
envincebal <vincentyan1@gmail.com>
2020-12-02 01:18:19 -0800

    Added Reverse Integer solution.

--- InterviewAlgorithmBootcamp/exercises/reverseint/index.js
+++ InterviewAlgorithmBootcamp/exercises/reverseint/index.js
@@ -1,13 +1,24 @@
-// --- Directions
-// Given an integer, return an integer that is the reverse
-// ordering of numbers.
-// --- Examples
-//   reverseInt(15) === 51
-//   reverseInt(981) === 189
-//   reverseInt(500) === 5
-//   reverseInt(-15) === -51
-//   reverseInt(-90) === -9
+// --- Directions Given an integer, return an integer that is the reverse
+// ordering of numbers. --- Examples   reverseInt(15) === 51   reverseInt(981)
+// === 189   reverseInt(500) === 5   reverseInt(-15) === -51   reverseInt(-90)
+// === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+  return parseInt(reversed) * Math.sign(n);
+}
+
+// Test cases
+let a =  reverseInt(51);
+let b = reverseInt(-51);
+let c = reverseInt(-90);
+
+console.log(a);
+console.log(b);
+console.log(c);
 
 module.exports = reverseInt;

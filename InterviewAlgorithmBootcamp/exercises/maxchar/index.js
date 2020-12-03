f127233b35b840ce8280666cb3ffee963fdd0455
envincebal <vincentyan1@gmail.com>
2020-12-03 11:48:32 -0800

    Added MaxChar solution.

--- InterviewAlgorithmBootcamp/exercises/maxchar/index.js
+++ InterviewAlgorithmBootcamp/exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var max = 0,
+     maxCharacter = "";
+  str.split("").forEach(function(char){
+    if(str.split(char).length > max) {
+        max = str.split(char).length;
+        maxCharacter = char;
+     }
+  });
+  return maxCharacter;
+}
+
+// Test cases
+let a =  maxChar("abcccccccd");
+let b = maxChar("apple 1231111");
+let c = maxChar("dhsjfhfdf");
+
+console.log(a);
+console.log(b);
+console.log(c);
 
 module.exports = maxChar;

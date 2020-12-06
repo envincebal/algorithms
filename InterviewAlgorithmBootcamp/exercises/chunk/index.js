03a386816de626a7088ad032fec199577a2d7b90
envincebal <vincentyan1@gmail.com>
2020-12-06 12:44:04 -0800

    Added chunk solution.

--- InterviewAlgorithmBootcamp/exercises/chunk/index.js
+++ InterviewAlgorithmBootcamp/exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {  
+  const newArr = [];
+
+  while(array.length){
+    newArr.push(array.slice(0, size));
+    array.splice(0, size);
+  }
+  return newArr;
+}
+
+let a = chunk([1, 2, 3, 4], 2);
+let b = chunk([1, 2, 3, 4, 5, 6, 7, 8], 3);
+let c = chunk([1, 2, 3, 4, 5], 10);
+
+console.log(a);
+console.log(b);
+console.log(c);
 
 module.exports = chunk;

a4840a68452d354ff3f014d26b91f78ef7811c72
envincebal <vincentyan1@gmail.com>
2020-12-26 20:12:02 -0800

    Added Steps solution.

--- InterviewAlgorithmBootcamp/exercises/steps/index.js
+++ InterviewAlgorithmBootcamp/exercises/steps/index.js
@@ -5,18 +5,40 @@
 // step has spaces on the right hand side!
 // --- Examples
 //   steps(2)
-//       '# '
-//       '##'
+//       "# "
+//       "##"
 //   steps(3)
-//       '#  '
-//       '## '
-//       '###'
+//       "#  "
+//       "## "
+//       "###"
 //   steps(4)
-//       '#   '
-//       '##  '
-//       '### '
-//       '####'
+//       "#   "
+//       "##  "
+//       "### "
+//       "####"
 
-function steps(n) {}
+function steps(n) {
+    for (let row = 0; row < n; row++) {
+    let stair = "";
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
+
+// Test cases
+let a = steps(3);
+let b = steps(5);
+let c = steps(10);
+
+console.log(a);
+console.log(b);
+console.log(c)
 
 module.exports = steps;
